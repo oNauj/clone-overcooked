73b17b21552a5461851a5923624ddeffbd51cd8b
Juan Matheus Braz Freire <juanmatheusbrazfreire@gmail.com>
2023-03-13 23:40:28 -0300

    add playeranimation

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 6f;
     [SerializeField] private float rotationSpeed = 9f;
+
+    private bool isWalking;
     private void Update()
     {
         Vector2 inputVector = GetMoveDir();
@@ -46,6 +48,12 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         this.transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
