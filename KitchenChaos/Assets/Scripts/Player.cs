b8e87ed1e76b4be51ac1d840c14ae36f13b03c49
Juan Matheus Braz Freire <juanmatheusbrazfreire@gmail.com>
2023-03-14 00:58:04 -0300

    upd charactercontroller add Input Manager

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -4,44 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] GameInput gameInput;
     [SerializeField] private float moveSpeed = 6f;
     [SerializeField] private float rotationSpeed = 9f;
 
     private bool isWalking;
     private void Update()
     {
-        Vector2 inputVector = GetMoveDir();
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         MovePosition(inputVector);
     }
 
-    private Vector2 GetMoveDir()
-    {
-        Vector2 inputVector = new Vector2(0f, 0f);
-        bool isKeyDown_W = Input.GetKey(KeyCode.W);
-        bool isKeyDown_S = Input.GetKey(KeyCode.S);
-        bool isKeyDown_A = Input.GetKey(KeyCode.A);
-        bool isKeyDown_D = Input.GetKey(KeyCode.D);
 
-        if (isKeyDown_W)
-        {
-            inputVector.y += 1f;
-        }
-        else if (isKeyDown_S)
-        {
-            inputVector.y -= 1f;
-        }
-        if (isKeyDown_A)
-        {
-            inputVector.x -= 1f;
-        }
-        else if (isKeyDown_D)
-        {
-            inputVector.x += 1f;
-        }
-
-        return inputVector;
-    }
 
     private void MovePosition(Vector2 inputVector)
     {
